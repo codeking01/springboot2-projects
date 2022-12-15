33bf8a8b12ee97d75aeea5f55f2ccfb80b61f275
codeking <jialiang_xiong@163.com>
2022-12-15 16:58:22 +0800

    2022.12.15, master commit,引入jdbcTemplate,简单做了测试
    Signed-off-by: codeking <1094234526@qq.com>

--- springboot-admin/src/test/java/com/codeking/admin/SpringbootAdminApplicationTests.java
+++ springboot-admin/src/test/java/com/codeking/admin/SpringbootAdminApplicationTests.java
@@ -1,13 +1,27 @@
 package com.codeking.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class SpringbootAdminApplicationTests {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void jdbcTemplateTest(){
+        // jdbcTemplate.queryForObject("select * from account_tbl")
+        // jdbcTemplate.queryForList("select * from account_tbl",)
+        Long aLong = jdbcTemplate.queryForObject("select count(*) from `user`", Long.class);
+        log.info("aLong: " + aLong);
+    }
 }
