dfc298cb6090e80fcd2a49c436f06981a0811b7d
codeking <jialiang_xiong@163.com>
2022-12-20 20:06:54 +0800

    2022.12.20, master commit,初步搭建这个项目
    Signed-off-by: codeking <1094234526@qq.com>

---
+++ reggie_project/src/main/java/com/codeking/boot/ReggieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codeking.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReggieProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieProjectApplication.class, args);
+    }
+
+}
